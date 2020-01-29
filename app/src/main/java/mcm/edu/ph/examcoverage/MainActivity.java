84c4476c164e0cf62c8eed66f50277b001c062b0
Kiboot <kiboot@users.noreply.github.com>
2020-01-29 21:43:14 +0800

    Initial commit

---
+++ app/src/main/java/mcm/edu/ph/examcoverage/MainActivity.java
@@ -0,0 +1,19 @@
+package mcm.edu.ph.examcoverage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+/** all the imports from this point forward are added by me **/
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
